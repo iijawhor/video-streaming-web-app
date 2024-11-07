eecd497ac0a06533a4c79ca1f5ddaa0257d88842
iijawhor <jawhorali1@gmail.com>
2024-11-07 12:22:21 +0530

    Utils : how to handle api error and response

---
+++ src/utils/asynHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req.res.next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+// const asyncHandler = (fn)=>async(req,res,next)=>{ // using try/catch
+//     try {
+//         await fn(req,res,next)
+        
+//     } catch (error) {
+//         console.log();
+//         res.status(error.code|| 500).json({
+//             success:false,
+//             message:er.message
+//         })
+        
+//     }
+// }
